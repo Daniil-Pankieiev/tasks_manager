f5eadbb749d9854883f27dd134b92ecadbfe56aa
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-11-26 16:39:16 +0200

    Add index.html

---
+++ task/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from task.views import (
+    index
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    ]
+
+app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task.models import Task, Worker, TaskType, Position
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    num_positions = Position.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_positions": num_positions,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task/index.html", context=context)
